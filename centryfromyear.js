f78472e2b6bd610fd4fb45164f530922ab595bb8
RESILIENTOMAR <TEIXEIRAOMAR1@GMAIL.COM>
2023-03-31 09:44:54 -0400

    codewar update

--- centryfromyear.js
+++ centryfromyear.js
@@ -9,3 +9,22 @@ function century(year) {
     const centuryNumber = Math.ceil(year / 100);
     return centuryNumber;
 }
+/*In this kata, we will make a function to test whether a period is late.
+
+Our function will take three parameters:
+
+last - The Date object with the date of the last period
+
+today - The Date object with the date of the check
+
+cycleLength - Integer representing the length of the cycle in days
+
+Return true if the number of days passed from last to today is greater than cycleLength. Otherwise, return false.*/
+
+
+
+function periodIsLate(last, today, cycleLength) {
+    const oneDayMs = 24 * 60 * 60 * 1000; // number of milliseconds in a day
+    const daysPassed = Math.round((today.getTime() - last.getTime()) / oneDayMs);
+    return daysPassed > cycleLength;
+}
