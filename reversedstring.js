ce9c265ada354da708e5e8f01976f716c47f5e5a
RESILIENTOMAR <TEIXEIRAOMAR1@GMAIL.COM>
2023-04-05 21:27:06 -0400

    update codewars

---
+++ reversedstring.js
@@ -0,0 +1,6 @@
+//Complete the solution so that it reverses the string passed into it.
+
+function solution(str) {
+    let reversedString = str.split('').reverse().join('');
+    return reversedString;
+}
